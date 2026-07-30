c0ad7bcfc691f54c4eaf956632cab9137385c39b
Martinelli Isidorio <martinelli.sa@hotmail.com>
2026-07-29 21:36:25 -0300

    iniciando a automação do teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedido.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+
+  await page.goto('http://localhost:5173/')
+  //Checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  //Checkpoint
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-XDAXDP')
+
+  await page.getByTestId('search-order-button').click()
+
+  await expect(page.getByTestId('search-order-id')).toBeVisible()
+
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-XDAXDP')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+})
